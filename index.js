580ee379c33a6ad2429d1ccf8c82ae0e0881e9a0
Anri Gogiberidze <anri45gogiberidze@gmail.com>
2022-06-21 10:20:28 +0400

    Rounded numbers to 3 decimal places

--- index.js
+++ index.js
@@ -7,21 +7,24 @@ const massEl = document.getElementById('mass');
 document.getElementById('convert-btn').addEventListener('click', () => {
     let numInputValue = numInput.value;
 
+    // Length
     let meterToFeet = numInputValue * 3.281;
     let feetToMeter = numInputValue / 3.281;
     
     lengthEl.textContent = `
-    ${numInputValue} meters = ${meterToFeet} feet | ${numInputValue} feet = ${feetToMeter} meters`;
+    ${numInputValue} meters = ${meterToFeet.toFixed(3)} feet | ${numInputValue} feet = ${feetToMeter.toFixed(3)} meters`;
 
+    // Volume
     let literToGallon = numInputValue / 3.785;
     let gallonToLiter = numInputValue * 3.785;
 
     volumeEl.textContent = `
-    ${numInputValue} liters = ${literToGallon} gallons | ${numInputValue} gallons = ${gallonToLiter} liters`;
+    ${numInputValue} liters = ${literToGallon.toFixed(3)} gallons | ${numInputValue} gallons = ${gallonToLiter.toFixed(3)} liters`;
 
+    // Weight
     let kiloToPound = numInputValue * 2.205;
     let poundToKilo = numInputValue / 2.205;
 
     massEl.textContent = `
-    ${numInputValue} kilos = ${kiloToPound} pounds | ${numInputValue} pounds = ${poundToKilo} kilos`;
+    ${numInputValue} kilos = ${kiloToPound.toFixed(3)} pounds | ${numInputValue} pounds = ${poundToKilo.toFixed(3)} kilos`;
 });
